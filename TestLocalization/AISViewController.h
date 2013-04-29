2518c89af97e4c22e43c853466ca0e97084f8edd
Gregory Hill <gregory.hill@admins-MacBook-Pro.local>
2013-04-29 14:54:15 -0400

    initial commit

---
+++ TestLocalization/AISViewController.h
@@ -0,0 +1,18 @@
+//
+//  AISViewController.h
+//  TestLocalization
+//
+//  Created by Gregory Hill on 4/15/13.
+//  Copyright (c) 2013 Applied IS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AISViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *localizedTextField;
+
+
+- (IBAction)buttonPressed:(id)sender;
+
+@end
